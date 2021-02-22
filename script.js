129ae20203833341b6ee67dd4734f7800789c4a5
Stephen Thompson <thompson.stephen33@gmail.com>
2021-02-21 20:34:42 -0500

    started implementing code for operations

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+let currentNum = '', num1 = '', num2 = '', operator = '';
+const allClear = document.getElementById('AC');
+allClear.addEventListener('click', () => clearEverything());
+
 const operators = Array.from(document.getElementsByClassName('operators'));
 operators.forEach(btn => {
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', (e) => {
         clearButtons();
+        operator = e.target.textContent;
+        num1 = currentNum;
+        currentNum = '';
         btn.classList.add('white-background');
     });
 });
@@ -10,6 +17,9 @@ const equals = document.getElementById('equals');
 equals.addEventListener('click', () => {
     clearButtons();
     equals.classList.add('equals-background');
+    num2 = currentNum;
+    currentNum = '';
+    operate(num1, num2);
 });
 equals.addEventListener('animationend', () => {
     equals.classList.remove('equals-background');
@@ -17,9 +27,11 @@ equals.addEventListener('animationend', () => {
 
 const numbers = Array.from(document.getElementsByClassName('numbers'));
 numbers.forEach(btn => {
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', (e) => {
         clearButtons();
         btn.classList.add('numbers-background');
+        if(currentNum.length < 9) currentNum += e.target.textContent;
+        currentDisplay(currentNum);
     });
     btn.addEventListener('animationend', () => {
         btn.classList.remove('numbers-background');
@@ -37,9 +49,47 @@ topBtns.forEach(btn => {
     })
 });
 
-function clearButtons () {
+const current = document.getElementById('current');
+const formula = document.getElementById('formula');
+
+function currentDisplay(num) {
+  current.textContent = num;
+}
+
+function clearEverything() {
+    clearButtons();
+    currentNum = '';
+    num1 = '';
+    num2 = '';
+    operator = '';
+    current.textContent = 0;
+}
+
+function clearButtons() {
     operators.forEach(btn => {
-        //equals.classList.replace('equals-background', 'assignment');
         btn.classList.replace('white-background', 'operators');
     });
 }
+
+function operate(a, b) {
+    a = Number(a);
+    b = Number(b);
+
+    switch(operator) {
+        case '+':
+            currentDisplay(a + b);
+            break;
+        case '-':
+            currentDisplay(a - b);
+            break;
+        case 'x':
+            currentDisplay(a * b);
+            break;
+        case '/':
+            currentDisplay(a / b);
+            break;
+    }
+}
+
+
+
